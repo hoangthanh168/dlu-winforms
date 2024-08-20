c990707607ce5f015e1c77b81f90808676234df7
hoangthanh168 <hoangthanh1367@gmail.com>
2024-08-20 09:22:54 +0700

    chore update project structure and remove unused files

---
+++ thietkeform/FormChinh.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace thietkeform
+{
+    partial class FormChinh
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            bài1ToolStripMenuItem = new ToolStripMenuItem();
+            bài2ToolStripMenuItem = new ToolStripMenuItem();
+            bài3ToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { bài1ToolStripMenuItem, bài2ToolStripMenuItem, bài3ToolStripMenuItem });
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(61, 20);
+            toolStripMenuItem1.Text = "2212460";
+            // 
+            // bài1ToolStripMenuItem
+            // 
+            bài1ToolStripMenuItem.Name = "bài1ToolStripMenuItem";
+            bài1ToolStripMenuItem.Size = new Size(99, 22);
+            bài1ToolStripMenuItem.Text = "Bài 1";
+            // 
+            // bài2ToolStripMenuItem
+            // 
+            bài2ToolStripMenuItem.Name = "bài2ToolStripMenuItem";
+            bài2ToolStripMenuItem.Size = new Size(99, 22);
+            bài2ToolStripMenuItem.Text = "Bài 2";
+            // 
+            // bài3ToolStripMenuItem
+            // 
+            bài3ToolStripMenuItem.Name = "bài3ToolStripMenuItem";
+            bài3ToolStripMenuItem.Size = new Size(99, 22);
+            bài3ToolStripMenuItem.Text = "Bài 3";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(24, 56);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(475, 315);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(538, 56);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 135);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 37);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(52, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Xanh";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 62);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(40, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Đỏ";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 87);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(54, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Trắng";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(538, 222);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(538, 251);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(582, 214);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(582, 243);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // FormChinh
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "FormChinh";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem bài1ToolStripMenuItem;
+        private ToolStripMenuItem bài2ToolStripMenuItem;
+        private ToolStripMenuItem bài3ToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ thietkeform/FormChinh.cs
@@ -0,0 +1,15 @@
+namespace thietkeform
+{
+    public partial class FormChinh : Form
+    {
+        public FormChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
